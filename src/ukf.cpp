70ae1fc8b5bbcc18a788b59c01ee7be606efe09a
Akshay Khole <akshay@reversehack.in>
2018-04-28 19:43:12 -0700

    Filling out prediction

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -25,10 +25,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 1;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.1;
 
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
@@ -54,6 +54,14 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  n_x_ = 5;
+  n_aug_ = 7;
+  lambda_ = (3 - n_aug_);
+  x_ = VectorXd(n_x_);
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+  weights_ = VectorXd(2 * n_aug_ + 1);
+  P_ = MatrixXd(n_x_, n_x_);
 }
 
 UKF::~UKF() {}
@@ -83,6 +91,7 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+
 }
 
 /**
@@ -113,4 +122,32 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the radar NIS.
   */
+
+  // Set Weights
+  weights_(0) = lambda_ / (lambda_ + n_aug_);
+  for(int i = 1; i < (2 * n_aug_); ++i) {
+    weights_(i) = 0.5 / (n_aug_ + lambda_);
+  }
+
+  // predict state mean
+  x_.fill(0.0);
+  for(int i = 0; i < (2 * n_aug_ + 1); ++i) {
+    x_ = x_ + weights_(i) * Xsig_pred_.col(i);
+  }
+
+  // predict state covariance matrix
+  P_.fill(0.0);
+  for(int i = 0; i < (2 * n_aug_ + 1); ++i) {
+    VectorXd x_diff = Xsig_pred_.col(i) - x_;
+
+    while(x_diff(3) > M_PI) {
+      x_diff(3) -= 2 * M_PI;
+    }
+
+    while(x_diff(3) < -M_PI) {
+      x_diff(3) -= 2 * M_PI;
+    }
+
+    P_ = P+ + weights_(i) * x_diff_ * x_diff.transpose();
+  }
 }
