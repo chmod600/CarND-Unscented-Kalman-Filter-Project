630767d819fe4ddf609613d30d18751f6020f448
Akshay Khole <akshay@reversehack.in>
2018-04-29 13:09:49 -0700

    Fixes bugs

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -90,7 +90,6 @@ UKF::~UKF() {}
  * either radar or laser.
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-
   cout << endl << "Process Measurement";
 
   /**
@@ -130,6 +129,8 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     Prediction(delta_t);
     UpdateMeasurement(meas_package);
   }
+
+  cout << endl << "Process Measurement end";
 }
 
 /**
@@ -138,6 +139,8 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  * measurement and this one.
  */
 void UKF::Prediction(double delta_t) {
+  cout << endl << "Prediction" << endl;
+
   /**
      TODO:
 
@@ -145,11 +148,12 @@ void UKF::Prediction(double delta_t) {
      vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
 
-  // -------- Augment Generated Sigmas --------------------
+  // -------- Augmented Sigmas --------------------
   VectorXd x_aug = VectorXd(n_aug_);
   MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
   MatrixXd Xsig_aug = MatrixXd(n_aug_, (2 * n_aug_ + 1));
 
+  x_aug.fill(0.0);
   x_aug.head(5) = x_;
   x_aug(5) = 0;
   x_aug(6) = 0;
@@ -160,13 +164,26 @@ void UKF::Prediction(double delta_t) {
   P_aug(6, 6) = std_yawdd_ * std_yawdd_;
 
   MatrixXd L = P_aug.llt().matrixL();
-  Xsig_aug.col(0) = x_aug; // the mean position is at 0
+  cout << endl << "Prediction - sigma0" << endl;
+  MatrixXd sq_factor = sqrt(lambda_ + n_aug_) * L;
+  cout << endl << "Prediction - sigma1-1" << endl;
 
-  for(int i = 0; i < n_aug_; i++) {
-    VectorXd sq_factor = sqrt(lambda_ + n_aug_) * L.col(i);
-    Xsig_aug.col(i + 1) = x_aug + sq_factor;
-    Xsig_aug.col(i + 1 + n_aug_) = x_aug - sq_factor;
-  }
+  MatrixXd x_replicated = x_aug.rowwise().replicate(7);
+  Xsig_aug.block<7, 1>(0, 0) = x_aug;
+  Xsig_aug.block<7, 7>(0, 1) = x_replicated + sq_factor;
+  Xsig_aug.block<7, 7>(0, 8) = x_replicated - sq_factor;
+
+
+  // Xsig_aug.col(0) = x_aug; // the mean position is at 0
+
+  // cout << endl << "Prediction - sigma1" << endl;
+
+  // for(int i = 0; i < n_aug_; i++) {
+  //   Xsig_aug.col(i + 1)          = x_aug + sq_factor;
+  //   Xsig_aug.col(i + 1 + n_aug_) = x_aug - sq_factor;
+  // }
+
+  cout << endl << "Prediction - sigma" << endl;
 
   // -------- Predict Sigmas --------------------
   Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
@@ -174,43 +191,45 @@ void UKF::Prediction(double delta_t) {
   for(int i = 0; i < (2 * n_aug_ + 1); ++i) {
 
     // extract values for better readability
-    double p_x = Xsig_aug(0,i);
-    double p_y = Xsig_aug(1,i);
-    double v = Xsig_aug(2,i);
-    double yaw = Xsig_aug(3,i);
-    double yawd = Xsig_aug(4,i);
-    double nu_a = Xsig_aug(5,i);
-    double nu_yawdd = Xsig_aug(6,i);
-
+    double p_x      = Xsig_aug(0, i);
+    double p_y      = Xsig_aug(1, i);
+    double v        = Xsig_aug(2, i);
+    double yaw      = Xsig_aug(3, i);
+    double yawd     = Xsig_aug(4, i);
+    double nu_a     = Xsig_aug(5, i);
+    double nu_yawdd = Xsig_aug(6, i);
     double px_p, py_p;
+
     if(fabs(yawd) > 0.001) {
       px_p = p_x + v / yawd * (sin(yaw + yawd * delta_t) - sin(yaw));
-      py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
-    }
-    else {
-      px_p = p_x + v*delta_t*cos(yaw);
-      py_p = p_y + v*delta_t*sin(yaw);
+      py_p = p_y + v / yawd * (cos(yaw) - cos(yaw + yawd * delta_t));
+
+    } else {
+      px_p = p_x + v * delta_t * cos(yaw);
+      py_p = p_y + v * delta_t * sin(yaw);
     }
 
     double v_p = v;
     double yaw_p = yaw + yawd * delta_t;
     double yawd_p = yawd;
 
-    //add noise
+    // add noise
     px_p = px_p + 0.5 * nu_a * delta_t * delta_t * cos(yaw);
     py_p = py_p + 0.5 * nu_a * delta_t * delta_t * sin(yaw);
-    v_p = v_p + nu_a*delta_t;
+    v_p = v_p + nu_a * delta_t;
 
     yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
     yawd_p = yawd_p + nu_yawdd*delta_t;
 
-    //write predicted sigma point into right column
-    Xsig_pred_(0,i) = px_p;
-    Xsig_pred_(1,i) = py_p;
-    Xsig_pred_(2,i) = v_p;
-    Xsig_pred_(3,i) = yaw_p;
-    Xsig_pred_(4,i) = yawd_p;
+    // write predicted sigma point into right column
+    Xsig_pred_(0, i) = px_p;
+    Xsig_pred_(1, i) = py_p;
+    Xsig_pred_(2, i) = v_p;
+    Xsig_pred_(3, i) = yaw_p;
+    Xsig_pred_(4, i) = yawd_p;
   }
+
+  cout << endl << "Prediction - mean & covariance" << endl;
 
   // -------- Predict mean and covariance -------
   // Set Weights
@@ -236,9 +255,12 @@ void UKF::Prediction(double delta_t) {
 
     P_ = P_ + + weights_(i) * x_diff * x_diff.transpose();
   }
+
+  cout << endl << "Prediction end";
 }
 
 void UKF::UpdateMeasurement(MeasurementPackage meas_package) {
+  cout << endl << "Update measurement";
   VectorXd z;
   MatrixXd z_sig;
   VectorXd z_pred;
